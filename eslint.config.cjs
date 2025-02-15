344242a3b4fe0b72533d01558f81f87db494d10f
SUN GONGYU <gsun571@aucklanduni.ac.nz>
2025-02-15 17:36:39 +0800

    feat: 工程化配置初始化

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,31 +1,23 @@
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ["*.cjs", "*.mjs", "*.js", "demos/**/*"],
-  rules: {
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-redundant-type-constituents": "off",
-  },
-};
+const js = require('@eslint/js');
+const prettierConfig = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
+const globals = require('globals');
+
+module.exports = [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			sourceType: 'script', // for commonJS
+			globals: {
+				...globals.node
+			}
+		},
+		plugins: {
+			prettier: prettierPlugin
+		},
+		rules: {
+			...prettierConfig.rules, // Integrate Prettier rules to disable conflicting ESLint rules
+			'prettier/prettier': 'error' // Ensure Prettier formatting issues are reported as ESLint errors
+		}
+	}
+];
--- packages/test.js
+++ packages/test.js
@@ -1,1 +1,2 @@
 const a = 124;
+console.log(a);
